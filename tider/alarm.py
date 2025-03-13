51ff31547c367e63783bd90b88faa569f233a783
LotusRain <1161525789@qq.com>
2025-03-13 16:32:13 +0800

    feature: support upload file by WeChatGRoBot.

--- tider/alarm.py
+++ tider/alarm.py
@@ -2,6 +2,7 @@ import json
 import time
 import socket
 import warnings
+from abc import abstractmethod
 
 from tider.exceptions import DownloadError
 from tider.utils.log import get_logger
@@ -12,8 +13,8 @@ BASIC_MD_MESSAGE = """
 <font color="#FF0000">**Tider Alarm Message**</font>
 
 > message: <font color="#DC143C">{message}</font>
-> alarm_time: {alarm_time}
 > alarm_count: {count}
+> alarm_time: {alarm_time}
 > host: {host}
 > pid: {pid}
 > spider: <font color="#2b90d9">{spider}</font>
@@ -26,55 +27,64 @@ class Alarm:
     msg_type: text|markdown|html
     """
 
-    def __init__(self, stats, spidername, msg_type='text'):
-        self.stats = stats
-        self.spidername = spidername
-        self.msg_type = msg_type
-
+    def __init__(self, crawler):
+        self.crawler = crawler
+        self.stats = crawler.stats
+        self.spidername = crawler.spidername
+        self.msg_type = crawler.alarm_message_type
+        self.msg_formatter = {
+            'text': self.text_formatter,
+            'markdown': self.markdown_formatter,
+        }
         self._topics = {}
 
     @classmethod
     def from_crawler(cls, crawler):
-        return cls(
-            stats=crawler.stats,
-            msg_type=crawler.alarm_message_type,
-            spidername=crawler.spidername
-        )
+        return cls(crawler=crawler)
 
     def alarm(self, msg,  msg_type=None, extra_infos=None):
-        if msg not in self._topics:
-            self._topics[msg] = 0
-        self._topics[msg] += 1
+        if isinstance(msg, str):
+            if msg not in self._topics:
+                self._topics[msg] = 0
+            self._topics[msg] += 1
         msg_type = msg_type or self.msg_type
 
         infos = {
             'message': msg,
+            'count': self._topics.get(msg) or 1,
             'alarm_time': time.strftime('%Y-%m-%d %H:%M:%S'),
-            'count': self._topics[msg],
             'host': socket.gethostname(),
-            'pid': self.stats.get_value('pid'),
+            'pid': self.crawler.pid,
             'spider': self.spidername,
             'start_time': self.stats.get_value("time/start_time/format")
         }
-        extra_infos = dict(extra_infos) if extra_infos else {}
-        if extra_infos:
-            infos.update(extra_infos)
+        infos.update(dict(extra_infos) if extra_infos else {})
 
-        if msg_type == 'text':
-            message = json.dumps(infos, ensure_ascii=False)
-        elif msg_type == 'markdown':
-            message = BASIC_MD_MESSAGE
-            message = message.format(**infos)
-            for key in extra_infos:
-                value = extra_infos[key]
-                if not isinstance(value, str):
-                    value = json.dumps(value, ensure_ascii=False, indent=4)
-                message += f'> {key}: {value}\n'
-            message.strip('\n')
-        else:
-            raise ValueError(f"Not support message type `{msg_type}` right now")
+        try:
+            message = self.msg_formatter[msg_type](infos)
+        except KeyError:
+            raise ValueError(f"Not support message type `{msg_type}`")
         self.send_message(message, msg_type, infos)
 
+    def text_formatter(self, infos):
+        return json.dumps(infos, ensure_ascii=False, indent=4)
+
+    def markdown_formatter(self, infos):
+        message = BASIC_MD_MESSAGE
+        message = message.format(**infos)
+        reserved_keys = ('message', 'count', 'alarm_time', 'host', 'pid', 'spider', 'start_time')
+        for key, value in infos.items():
+            if key in reserved_keys:
+                continue
+            try:
+                value = json.dumps(value, ensure_ascii=False, indent=4)
+            except json.JSONDecodeError:
+                pass
+            message += f'> {key}: {value}\n'
+        message.strip('\n')
+        return message
+
+    @abstractmethod
     def send_message(self, message, msg_type=None, infos=None):
         warnings.warn(message)
 
@@ -84,32 +94,55 @@ class Alarm:
 
 class WeChatGRobotAlarm(Alarm):
 
-    def __init__(self, crawler, stats, spidername, key, mentioned_list=None,
-                 mentioned_mobile_list=None, msg_type='text'):
-        super().__init__(stats, spidername, msg_type)
+    def __init__(self, crawler, robot_key, mentioned_list=None, mentioned_mobile_list=None):
+        super().__init__(crawler=crawler)
         self.crawler = crawler
-        self.key = key
+        self.robot_key = robot_key
         self.mentioned_list = mentioned_list
         self.mentioned_mobile_list = mentioned_mobile_list
+
+        self.msg_formatter.update({'file': self.file_formatter})
 
     @classmethod
     def from_crawler(cls, crawler):
         return cls(
             crawler=crawler,
-            stats=crawler.stats,
-            msg_type=crawler.settings['ALARM_MESSAGE_TYPE'],
-            spidername=crawler.spidername,
-            key=crawler.settings["WECHAT_ROBOT_KEY"],
+            robot_key=crawler.settings["WECHAT_ROBOT_KEY"],
             mentioned_list=crawler.settings.getlist("WECHAT_ROBOT_MENTIONED_LIST"),
             mentioned_mobile_list=crawler.settings.getlist("WECHAT_ROBOT_MENTIONED_MOBILE_LIST")
         )
 
+    def file_formatter(self, infos):
+        media_id = ""
+        content = infos.get('message')
+        filename = infos.get('filename') or ""
+        url = f"https://qyapi.weixin.qq.com/cgi-bin/webhook/upload_media?key={self.robot_key}&type=file"
+        files = {
+            'media': (filename, content),
+        }
+        explorer = self.crawler.engine.explorer
+        try:
+            response = explorer.try_explore(method="POST", url=url, files=files, max_retries=2)
+            media_id = response.json()['media_id']
+            logger.info(f"Uploaded file to wechat robot: {media_id}")
+            response.close()
+        except DownloadError as e:
+            message = f"Failed to upload file: {e}"
+            logger.error(message)
+            infos['message'] = message
+        return media_id
+
     def send_message(self, message, msg_type=None, infos=None):
-        url = f"https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key={self.key}"
+        url = f"https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key={self.robot_key}"
+        if not message:
+            # failed to upload file.
+            message = self.markdown_formatter(infos)
+            msg_type = 'markdown'
+        content_key = 'content' if msg_type != 'file' else 'media_id'
         payload = {
             "msgtype": msg_type,
             msg_type: {
-                "content": message,
+                content_key: message,
                 "mentioned_list": self.mentioned_list,
                 "mentioned_mobile_list": self.mentioned_mobile_list
             }
