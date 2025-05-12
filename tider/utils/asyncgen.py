2c5f13c317c8a5eb856a6bbddbd41f8a70d17d91
LotusRain <1161525789@qq.com>
2025-05-12 14:57:26 +0800

    feature: async generator.

---
+++ tider/utils/asyncgen.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+from collections.abc import AsyncGenerator, AsyncIterable, Iterable
+from typing import TypeVar, Union
+
+_T = TypeVar("_T")
+
+
+async def collect_asyncgen(result: AsyncIterable[_T]) -> list[_T]:
+    return [x async for x in result]
+
+
+async def as_async_generator(
+    it: Union[Iterable[_T], AsyncIterable[_T]]
+) -> AsyncGenerator[_T]:
+    """Wraps an iterable (sync or async) into an async generator."""
+    if isinstance(it, AsyncIterable):
+        async for r in it:
+            yield r
+    else:
+        for r in it:
+            yield r
