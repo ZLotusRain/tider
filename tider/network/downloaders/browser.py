330898422a35bee6bbf0ee4f849ed887f21fce3f
LotusRain <1161525789@qq.com>
2025-05-16 15:22:04 +0800

    feature: optimize rendering page and caching static resources.

--- tider/network/downloaders/browser.py
+++ tider/network/downloaders/browser.py
@@ -3,6 +3,7 @@ import os.path
 import warnings
 import functools
 from threading import RLock
+from collections import defaultdict
 
 try:
     from playwright.sync_api import sync_playwright, TimeoutError as PlaywrightTimeoutError
@@ -28,6 +29,7 @@ logger = get_logger(__name__)
 INIT_SCRIPT = """
     window.elementsCreatedByVue = [];
     window.elementsCreatedByDOM = [];
+    window._hooked = []
     
     document._createElement = document.createElement;
     document.createElement = function (name) {
@@ -41,8 +43,8 @@ INIT_SCRIPT = """
     };
     
     waitFiveSeconds = async function() {
-        console.log("Starting to wait for 10 seconds...");
-        await delay(10000);
+        console.log("Starting to wait for 5 seconds...");
+        await delay(5000);
         console.log("5 seconds have passed!");
         return "Completed";
     };
@@ -73,14 +75,16 @@ INIT_SCRIPT = """
     }
     
     createHidden = function(name) {
-        let newInput = window.document.createElement('input');
+        if (window._hooked.includes(name)) { return; }
+        window._hooked.push(name); 
+        let newInput = document.createElement('input');
         newInput.type = 'hidden'
         if (name != "") {
             newInput.id = `foo-tider-${name}`;
         } else { newInput.id = 'foo-tider'; }
         
         newInput.value = 'DOM_LOAD_HOOK'
-        let lastElement = window.document.body.lastElementChild;
+        let lastElement = (document?.body || document?.head || document).lastElementChild;
         lastElement.insertAdjacentElement('afterend', newInput);
     }
     
@@ -124,17 +128,20 @@ STYLE_PATCH = """
 """
 
 VUE_DOM_LOAD_HOOK = """
-this.waitFiveSeconds().then(result => {{
+(this || window).waitFiveSeconds().then(result => {{
     console.log(result);
 }});
 
-setTimeout(this.createHidden("{name}"), 1000);
-
-// const newParagraph_{name} = window.document.createElement('p');
-// newParagraph_{name}.id = 'foo-tider-{name}';
-// newParagraph_{name}.textContent = 'DOM_LOAD_HOOK';
-// const lastElement_{name} = window.document.body.lastElementChild;
-// lastElement_{name}.insertAdjacentElement('afterend', newParagraph_{name});
+// const executeOnce = (function() {{
+//     let executed = false;
+//     return function() {{
+//         if (executed) {{ return;}}
+//         setTimeout(this.createHidden("{name}"), 1000);
+//         executed = true;
+//     }};
+// }})();
+// executeOnce();
+setTimeout((this || window).createHidden("{name}"), 1000);
 """
 
 LAST_SCRIPT = """setTimeout(this.createHidden(""), 1000);"""
@@ -142,7 +149,7 @@ LAST_SCRIPT = """setTimeout(this.createHidden(""), 1000);"""
 DOM_LOAD_HOOK = f"""
     const newParagraph = document.createElement('script');
     newParagraph.textContent = '{LAST_SCRIPT}';
-    const lastElement = document.body.lastElementChild;
+    const lastElement = (document?.body || document?.head || document).lastElementChild;
     lastElement.insertAdjacentElement('afterend', newParagraph);
 """
 
@@ -184,17 +191,24 @@ class BrowserDownloader:
     @classmethod
     def from_crawler(cls, crawler):
         return cls(
-            cache_statics=crawler.settings.getbool('EXPLORER_BROWSER_CACHE_STATICS', True),
-            cache_dir=crawler.settings.get('EXPLORER_BROWSER_CACHE_DIR'),
+            cache_statics=crawler.settings.getbool('DOWNLOADER_BROWSER_CACHE_STATICS', True),
+            cache_dir=crawler.settings.get('DOWNLOADER_BROWSER_CACHE_DIR'),
         )
 
-    def get_static_name(self, url):
+    @staticmethod
+    def get_static_name(url):
+        if "?" in url:
+            url, _ = url.split('?')
         parts = url.split('/')
         if parts == 1:
             name = parts[0]
         else:
             name = ".".join((parts[-2], parts[-1]))
+        # name = re.sub(r'[\\/:*?<>|"]+', '_', name)
         return name
+
+    def download_request(self, request: Request, **_):
+        pass
 
     def close_expired_connections(self):
         pass
@@ -216,50 +230,51 @@ class PlaywrightDownloader(BrowserDownloader):
     def handle_route(self, route, *_, hooked, banned_routes=None):
         # arg_count = len(inspect.signature(handler).parameters)
         name = self.get_static_name(route.request.url)
+        cached_js = self._cache_dir / parse_url_host(route.request.url) / name
         if route.request.resource_type in banned_routes:
             route.abort()
-        elif name.endswith('.js'):
-            cached_js = self._cache_dir / parse_url_host(route.request.url) / name
+        elif name.endswith('.js') or route.request.resource_type == 'script':
+            frame = route.request.frame
+            hooking = False
+            if not name.endswith('.min.js') and 'element-ui' not in name and 'ajax.js' not in name:
+                # include iframes.
+                if frame.url != 'about:blank' and frame.page.url != 'about:blank':
+                    hooking = True
+                    hooked[frame].append(name)
             original_response = None
-            headers = {}
+            # Failed to load module script: Expected a JavaScript module script but the server responded with
+            # a MIME type of "". Strict MIME type checking is enforced for module scripts per HTML spec.
+            headers = {"content-type": "application/javascript; charset=utf-8"}  # vue?
             # original_len = original_response.headers["content-length"]
             if os.path.exists(cached_js):
                 with open(cached_js, 'rb') as fo:
                     body = fo.read()
             else:
                 original_response = route.fetch()
-                headers = original_response.headers
+                headers.update(original_response.headers)
                 body = original_response.body()
-            if not name.endswith('.min.js') and 'element-ui' not in name and 'ajax.js' not in name:
+            if hooking:
                 body += b'\n' + VUE_DOM_LOAD_HOOK.format(name=set_md5(name)).encode('utf-8')
-                hooked.append(name)
-            elif name.endswith('vue.min.js'):
-                source = b"""$createElement=function(e,n,r,i){return"""
-                repl = b"""$createElement = function(e, n, r, i) {
-                    if (typeof e === "string") {
-                        window.elementsCreatedByVue.push({
-                            name: e,
-                            attrs: n,
-                            r: r,
-                            i: i
-                     })
-                    }
-                    return"""
-                body = body.replace(source, repl)
+            # maybe replace $createElement in vue.min.js
             route.fulfill(
                 response=original_response,
                 body=body,
                 headers={**headers}
             )
+        elif os.path.exists(cached_js):
+            with open(cached_js, 'rb') as fo:
+                body = fo.read()
+            route.fulfill(body=body)
         else:
             route.continue_()
 
     def on_response(self, response):
-        if not self._cache_statics:
+        if not self._cache_statics or response.request.method != 'GET':
             return
-        if response.request.method != 'GET' or response.request.resource_type == 'xhr':
+        if response.request.resource_type in ('xhr', 'document', 'fetch'):
             return
-        if not os.path.exists(self._cache_dir / parse_url_host(response.request.url)):
+        if response.frame.page.is_closed():
+            # don't try to fetch response.body
             return
 
         name = self.get_static_name(response.request.url)
@@ -275,6 +290,22 @@ class PlaywrightDownloader(BrowserDownloader):
                 with open(cached_path, 'wb') as fo:
                     fo.write(body)
 
+    @staticmethod
+    def _wait_for_hook(frame, name=None, timeout=0):
+        identity = f"foo-tider-{set_md5(name)}" if name else "foo-tider"
+        frame.locator(f"#{identity}").wait_for(state='attached', timeout=timeout)  # appropriately increase
+        frame.locator(f"#{identity}").evaluate("(ele) => {if (ele) ele.remove()}")
+        frame.wait_for_timeout(300)
+
+    def wait_for_hooked_frames(self, hooked, timeout=0, wait_for_load=False):
+        for frame in hooked:
+            hooks = hooked[frame]
+            if frame.url == frame.page.url and wait_for_load:
+                hooks.append("")
+            while hooks:
+                hook = hooks.pop()
+                self._wait_for_hook(frame=frame, name=hook, timeout=timeout)
+
     def download_request(self, request: Request, **_):
         # https://github.com/microsoft/playwright/issues/27997
         browser_type = request.meta.get('browser_type', 'chromium')
@@ -288,8 +319,11 @@ class PlaywrightDownloader(BrowserDownloader):
         init_script = request.meta.get('browser_init_script')
         init_script_path = request.meta.get('browser_init_script_path')
         device = request.meta.get('browser_device')
+        scroll_to_bottom = request.meta.get('browser_scroll_to_bottom', False)
+        scroll_timeout = request.meta.get('browser_scroll_timeout', request.timeout)
 
-        playwright_context = browser = None
+        playwright_context = browser = page = None
+        hooked = defaultdict(list)
         try:
             playwright_context = sync_playwright()
             playwright = playwright_context.start()
@@ -310,12 +344,11 @@ class PlaywrightDownloader(BrowserDownloader):
             page.add_init_script(script=INIT_SCRIPT)
             if init_script or init_script_path:
                 page.add_init_script(script=init_script, path=init_script_path)
-            page.set_extra_http_headers(request.headers)
+            page.set_extra_http_headers(dict(request.headers))
 
             banned_routes = ['media', 'other', 'websocket']
             if not screenshot_path:
                 banned_routes.append('image')
-            hooked = []
             page.on('response', self.on_response)
             page.route("**/*", functools.partial(self.handle_route, hooked=hooked, banned_routes=banned_routes))
 
@@ -323,21 +356,32 @@ class PlaywrightDownloader(BrowserDownloader):
             # automatically dismiss dialogs.
             browser_response = page.goto(request.url, wait_until='networkidle')
             page.add_script_tag(content=DOM_LOAD_HOOK)
-            hooked.append("")
+            self.wait_for_hooked_frames(hooked, timeout=request.timeout * 1000, wait_for_load=True)
 
-            for hook in hooked:
-                if hook == '':
-                    identity = "foo-tider"
-                else:
-                    identity = f"foo-tider-{set_md5(hook)}"
-                page.locator(f"#{identity}").wait_for(state='attached', timeout=request.timeout * 1000)
-                page.locator(f"#{identity}").evaluate("(ele) => {if (ele) ele.remove()}")
+            # https://www.reddit.com/
+            # https://www.steelwood.amsterdam/
             current_position = 0
+            history_heights = []
             page_height = page.evaluate("document.body.scrollHeight")
-            while current_position < page_height:
-                current_position += 300
-                page.evaluate(f"window.scrollTo(0, {current_position})")
-                page.wait_for_timeout(300)
+            scroll_start = preferred_clock()
+            while True:
+                history_heights.append(page_height)
+                history_heights = history_heights[-5:]  # Keep only the last 5 heights, to not run out of memory
+                while current_position < page_height:
+                    current_position += 300
+                    # page.mouse.wheel(0, current_position)
+                    page.evaluate(f"window.scrollTo(0, {current_position})")
+                    page.wait_for_timeout(300)
+                if not scroll_to_bottom:
+                    break
+                if scroll_to_bottom and history_heights and history_heights[-1] == page_height:
+                    break
+                scroll_elapsed = preferred_clock() - scroll_start
+                if scroll_timeout:
+                    if scroll_elapsed > scroll_timeout or len(history_heights) == 5 and len(set(history_heights)) == 1:
+                        break
+            page.wait_for_load_state('networkidle')
+            self.wait_for_hooked_frames(hooked, timeout=request.timeout * 1000)
 
             html = page.content()
             html = html.replace(DOM_LOAD_HOOK, '').replace(LAST_SCRIPT, '')
@@ -360,11 +404,16 @@ class PlaywrightDownloader(BrowserDownloader):
         except Exception as e:
             if 'ERR_TUNNEL_CONNECTION_FAILED' in str(e):
                 request.invalidate_proxy()
+            if 'ERR_INVALID_AUTH_CREDENTIALS' in str(e):
+                request.invalidate_proxy()
             response = Response(request)
             if not response.failed:  # maybe already failed in response.read().
                 response.fail(error=e)
             return response
         finally:
+            if page is not None:
+                page.unroute_all(behavior='ignoreErrors')
+            hooked.clear()
             browser and browser.close()
             playwright_context and playwright_context.__exit__()
 
