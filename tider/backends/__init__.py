2129fd4e7340c80ee9a0a371f1f1184d4c20cc97
LotusRain <1161525789@qq.com>
2025-09-01 16:41:11 +0800

    feature: tider backends.

---
+++ tider/backends/__init__.py
@@ -0,0 +1,48 @@
+"""Crawler stats, errors and failures backends."""
+
+import sys
+import types
+
+from .base import BaseBackend, KeyValueStoreBackend, State, backend_serializer_registry
+
+from tider.utils.misc import symbol_by_name
+from tider.exceptions import reraise, ImproperlyConfigured
+
+__all__ = (
+    'backend_serializer_registry', 'State',
+    'BaseBackend', 'KeyValueStoreBackend', 'by_name'
+)
+
+
+BACKEND_ALIASES = {
+    'redis': 'tider.backends.redis:RedisBackend',
+    'mongodb': 'tider.backends.mongodb:MongoBackend',
+    'disabled': 'tider.backends.base:DisabledBackend',
+}
+
+
+def by_name(backend=None):
+    """Get backend class by name/alias."""
+    backend = backend or 'disabled'
+    try:
+        cls = symbol_by_name(backend, BACKEND_ALIASES)
+    except ValueError as exc:
+        reraise(ImproperlyConfigured, ImproperlyConfigured(
+            "Unknown result backend: {0!r}.".format(backend, exc)), sys.exc_info()[2])
+    if isinstance(cls, types.ModuleType):
+        raise ImproperlyConfigured("Unknown result backend: {0!r}.".format(
+            backend, 'is a Python module, not a backend class.'))
+    return cls
+
+
+def by_url(backend=None):
+    """Get backend class by URL."""
+    url = None
+    if backend and '://' in backend:
+        url = backend
+        scheme, _, _ = url.partition('://')
+        if '+' in scheme:
+            backend, url = url.split('+', 1)
+        else:
+            backend = scheme
+    return by_name(backend), url
