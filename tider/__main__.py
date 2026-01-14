c65b860f8855448966f9ce534016cf4ed31b2fda
LotusRain <1161525789@qq.com>
2026-01-14 10:53:31 +0800

    feature: don't patch anything before the child process is spawned.

--- tider/__main__.py
+++ tider/__main__.py
@@ -7,11 +7,20 @@ from . import maybe_patch_concurrency, maybe_patch_third_party
 __all__ = ('main',)
 
 
+def maybe_patch():
+    if 'multi' in sys.argv:
+        return
+    if '-D' in sys.argv:
+        return
+    if '--detach' in sys.argv:
+        return
+    maybe_patch_concurrency()
+    maybe_patch_third_party()
+
+
 def main():
     """Entrypoint to the ``tider`` umbrella command."""
-    if 'multi' not in sys.argv:
-        maybe_patch_concurrency()
-        maybe_patch_third_party()
+    maybe_patch()
     from tider.bin.tider import main as _main
     sys.exit(_main())
 
